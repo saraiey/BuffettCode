0362137d2bf53e58c3ffedf5df7b606ff38a1da7
saraiey <saraie-yuki@marimo-ai.co.jp>
2022-11-08 09:51:47 +0900

    途中経過

--- getStockData.py
+++ getStockData.py
@@ -1,10 +1,11 @@
 import json
-import pandas_datareader as pdr
+import pandas 
+import pandas_datareader.data as web
 import datetime
 
 # Ticker読み込む
 def read_ticker():
-    tickerfile = "./ticker.txt"
+    tickerfile = "./test.txt"
     tickers = open(tickerfile, "r")
     tickerslist = json.load(tickers)
     tickers.close
@@ -12,10 +13,15 @@ def read_ticker():
 
 if __name__ == "__main__":
     tickerslist = read_ticker()
-    stockTickerlist = [str(t)+'.JP' for t in tickerslist["ticker"]]
     startdate = datetime.date(2012,1,1)
     enddate = datetime.date.today()
-    df = pdr.DataReader(stockTickerlist, 'stooq', startdate, enddate)
+    df = []
+    for t in tickerslist["ticker"]:
+        ticker_symbol = [str(t)+'.JP']
+        df1 = web.DataReader(ticker_symbol, data_source='stooq', start=startdate, end=enddate)
+        df1 = df1.insert(0, "code", ticker_symbol, allow_duplicates=False)
+        df = df.append(df1)
     print(len(df.index))
-    stockData = df["Close"]
+    # stockData = df["Close"]
+    stockData = pandas.melt(df)
     stockData.to_csv("./stock_price_data.csv", encoding = "utf-8")
